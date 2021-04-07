dd14efc9ba2d175629fa61aa445e5079c1b6002f
Josep M Sobrepere <josepot@gmail.com>
2021-04-08 00:56:33 +0200

    Async currency-rates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import { bind, Subscribe } from "@react-rxjs/core"
 import { createKeyedSignal, createSignal, combineKeys } from "@react-rxjs/utils"
 import { memo } from "react"
-import { combineLatest, concat, EMPTY, pipe } from "rxjs"
-import { map, pluck, scan, switchMap } from "rxjs/operators"
+import {
+  combineLatest,
+  concat,
+  defer,
+  EMPTY,
+  merge,
+  Observable,
+  of,
+  OperatorFunction,
+  pipe,
+} from "rxjs"
+import {
+  connect,
+  delay,
+  distinctUntilChanged,
+  filter,
+  map,
+  mergeMap,
+  pluck,
+  repeat,
+  scan,
+  startWith,
+  switchMap,
+  take,
+  takeLast,
+  takeWhile,
+  withLatestFrom,
+} from "rxjs/operators"
 import {
   initialCurrencyRates,
   formatCurrency,
@@ -13,14 +39,85 @@ import {
   getBaseCurrencyPrice,
   uuidv4,
   getRandomOrder,
+  isCurrecyRateValid,
 } from "./utils"
 
 const [useCurrencies] = bind(EMPTY, Object.keys(initialCurrencyRates))
 
 const [rateChange$, onRateChange] = createKeyedSignal<string, number>()
+
+enum CurrencyRateState {
+  ACCEPTED,
+  DIRTY,
+  IN_PROGRESS,
+}
+
+interface CurrencyRate {
+  value: number
+  state: CurrencyRateState
+}
+
 const [useCurrencyRate, currencyRate$] = bind(
-  rateChange$,
-  (currency) => initialCurrencyRates[currency],
+  (currency: string): Observable<CurrencyRate> => {
+    const latestAcceptedValue$ = acceptedCurrencyRates$(currency).pipe(take(1))
+
+    const getNextAcceptedValue$ = (candidate: number) =>
+      defer(() => isCurrecyRateValid(currency, candidate)).pipe(
+        mergeMap((isOk) => (isOk ? of(candidate) : latestAcceptedValue$)),
+        map((value) => ({
+          value,
+          state: CurrencyRateState.ACCEPTED,
+        })),
+        startWith({ value: candidate, state: CurrencyRateState.IN_PROGRESS }),
+      )
+
+    const delayServerValidation = (): OperatorFunction<number, CurrencyRate> =>
+      pipe(
+        withLatestFrom(latestAcceptedValue$),
+        switchMap(([value, latestAccepted]) =>
+          value === latestAccepted
+            ? of({ value, state: CurrencyRateState.ACCEPTED })
+            : concat(
+                of({ value, state: CurrencyRateState.DIRTY }),
+                of(null).pipe(delay(500)),
+              ),
+        ),
+        takeWhile((x): x is CurrencyRate => !!x),
+      )
+
+    const validateRateCurrency = (): OperatorFunction<
+      CurrencyRate,
+      CurrencyRate
+    > =>
+      connect((source$) =>
+        merge(
+          source$,
+          source$.pipe(
+            takeLast(1),
+            mergeMap(({ value }) => getNextAcceptedValue$(value)),
+          ),
+        ),
+      )
+
+    return rateChange$(currency).pipe(
+      delayServerValidation(),
+      validateRateCurrency(),
+      repeat(),
+    )
+  },
+  (currency) => ({
+    state: CurrencyRateState.ACCEPTED,
+    value: initialCurrencyRates[currency],
+  }),
+)
+
+const [, acceptedCurrencyRates$] = bind(
+  pipe(
+    currencyRate$,
+    filter(({ state }) => state === CurrencyRateState.ACCEPTED),
+    pluck("value"),
+    distinctUntilChanged(),
+  ),
 )
 
 const initialOrderIds = Object.keys(initialOrders)
@@ -41,7 +138,7 @@ const [useOrder, order$] = bind((id: string) => {
   const price$ = concat([initialOrder.price], priceChange$(id))
   const currency$ = concat([initialOrder.currency], currencyChange$(id))
 
-  const rate$ = currency$.pipe(switchMap((ccy) => currencyRate$(ccy)))
+  const rate$ = currency$.pipe(switchMap((ccy) => acceptedCurrencyRates$(ccy)))
   const baseCurrencyPrice$ = combineLatest([price$, rate$]).pipe(
     map(([price, rate]) => getBaseCurrencyPrice(price, rate)),
   )
@@ -59,17 +156,24 @@ const [useTotal] = bind(
   ),
 )
 
-const CurrencyRate: React.FC<{ currency: string }> = ({ currency }) => {
-  const rate = useCurrencyRate(currency)
+const CurrencyRateRow: React.FC<{ currency: string }> = ({ currency }) => {
+  const currencyRate = useCurrencyRate(currency)
   return (
     <tr key={currency}>
       <td>{formatCurrency(currency)}</td>
       <td>
         <NumberInput
-          value={rate}
+          value={currencyRate.value}
           onChange={(value) => {
             onRateChange(currency, value)
           }}
+          style={{
+            backgroundColor:
+              currencyRate.state === CurrencyRateState.ACCEPTED
+                ? "limegreen"
+                : undefined,
+          }}
+          disabled={currencyRate.state === CurrencyRateState.IN_PROGRESS}
         />
       </td>
     </tr>
@@ -81,7 +185,7 @@ const Currencies = () => {
   return (
     <Table columns={["Currency", "Exchange rate"]}>
       {currencies.map((currency) => (
-        <CurrencyRate key={currency} currency={currency} />
+        <CurrencyRateRow key={currency} currency={currency} />
       ))}
     </Table>
   )
